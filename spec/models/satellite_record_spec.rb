0d90b4f19d1d28cf3dce271e80c8ef3d43e6e747
pawlkris <paulkristapovich@gmail.com>
2018-03-15 09:34:04 -0400

    last few tweaks to tests

--- spec/models/satellite_record_spec.rb
+++ spec/models/satellite_record_spec.rb
@@ -7,9 +7,13 @@ RSpec.describe SatelliteRecord, type: :model do
       6.times {|i| SatelliteRecord.create(altitude: 120, last_updated:"2018-03-13 13:16:00")}
       expect(SatelliteRecord.minute_below?(SatelliteRecord.last)).to eq(true)
     end
-    it "returns false when at least one of six records have average altitude below 160" do
-      SatelliteRecord.create(altitude: 180, last_updated:"2018-03-13 13:16:00")
-      5.times {|i| SatelliteRecord.create(altitude: 120, last_updated:"2018-03-13 13:16:00")}
+    it "returns false when fewer than six records exist" do
+      4.times {|i| SatelliteRecord.create(altitude: 120, last_updated:"2018-03-13 13:16:00")}
+      expect(SatelliteRecord.minute_below?(SatelliteRecord.last)).to eq(false)
+    end
+    it "returns false when at least one of six records have average altitude above 160" do
+      6.times {|i| SatelliteRecord.create(altitude: 158, last_updated:"2018-03-13 13:16:00")}
+      SatelliteRecord.create(altitude: 300, last_updated:"2018-03-13 13:16:00")
       expect(SatelliteRecord.minute_below?(SatelliteRecord.last)).to eq(false)
     end
   end
--- spec/models/satellite_reporter_spec.rb
+++ spec/models/satellite_reporter_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe SatelliteReporter, type: :model do
 
   describe "SatelliteReporter.health" do
     it "returns 'WARNING: RAPID ORBITAL DECAY IMMINENT' when previous 7 records' averages are below 160, thus making the last record's minute_below status true" do
-      6.times {|i| SatelliteRecord.create(altitude: 120, last_updated:"2018-03-13 13:16:00")}
+      7.times {|i| SatelliteRecord.create(altitude: 120, last_updated:"2018-03-13 13:16:00")}
       SatelliteRecord.create(altitude: 120.757129951577, last_updated:"2018-03-13 13:16:00")
       reporter = SatelliteReporter.new
       expect(reporter.health).to eq({:message=>"WARNING: RAPID ORBITAL DECAY IMMINENT"} )
@@ -20,7 +20,7 @@ RSpec.describe SatelliteReporter, type: :model do
 
     it "returns 'Sustained Low Earth Orbit Resumed' when at least one of the past seven records' altitudes is above 160 at least one record's minute_below? status is true" do
       7.times{|i| SatelliteRecord.create(altitude: 158.757129951577, last_updated:"2018-03-13 13:16:00")}
-      2.times{|i| SatelliteRecord.create(altitude: 250.757129951577, last_updated:"2018-03-13 13:16:00")}
+      1.times{|i| SatelliteRecord.create(altitude: 250.757129951577, last_updated:"2018-03-13 13:16:00")}
       reporter = SatelliteReporter.new
       expect(reporter.health).to eq({:message=>"Sustained Low Earth Orbit Resumed"})
     end
@@ -30,7 +30,7 @@ RSpec.describe SatelliteReporter, type: :model do
     it "returns 'Altitude is A-OK' when one average is below 160, but others are above and no records have minute below equal to true" do
       sat_info = {altitude: 166.757129951577, last_updated:"2018-03-13 13:16:00"}
       SatelliteRecord.create(altitude: 120.757129951577, last_updated:"2018-03-13 13:16:00")
-        7.times {|i| SatelliteRecord.create(sat_info)}
+        6.times {|i| SatelliteRecord.create(sat_info)}
         reporter = SatelliteReporter.new
       expect(reporter.health).to eq({:message=>"Altitude is A-OK"} )
     end
